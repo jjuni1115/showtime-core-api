21f5101625518722da2fb699cc827d75b46d7903
jjuni1115 <junyege@naver.com>
2025-04-08 14:46:57 +0900

    fix: 로깅 필터 로직 변경

--- src/main/java/com/showtime/coreapi/logger/LoggingInterceptor.java
+++ src/main/java/com/showtime/coreapi/logger/LoggingInterceptor.java
@@ -32,15 +32,21 @@ public class LoggingInterceptor implements HandlerInterceptor {
             log.info("Params: [{}]",getRequestParam(request));
         }
 
-        CustomHttpRequestWrapper requestWrapper = (CustomHttpRequestWrapper) request;
+        if(request instanceof CustomHttpRequestWrapper){
 
-        if(requestWrapper.getRequestBody()!=null) {
-            String requestBody = new String(requestWrapper.getRequestBody());
+            CustomHttpRequestWrapper requestWrapper = (CustomHttpRequestWrapper) request;
 
-            if (request != null && requestBody.length() > 0) {
-                log.info("Request Body: [{}]", requestBody);
+            if(requestWrapper.getRequestBody()!=null) {
+                String requestBody = new String(requestWrapper.getRequestBody());
+
+                if (request != null && requestBody.length() > 0) {
+                    log.info("Request Body: [{}]", requestBody);
+                }
             }
+
         }
+
+
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
@@ -54,11 +60,18 @@ public class LoggingInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
         log.info("Response Status: [{}]",response.getStatus());
-        byte[] responseBody = ((CustomHttpResponseWrapper) response).getResponseBody();
 
-        if(responseBody!=null && responseBody.length>0){
-            log.info("Response Body: [{}]", new String(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectMapper.readValue(responseBody,Object.class))));
+        if(response instanceof CustomHttpResponseWrapper){
+
+            byte[] responseBody = ((CustomHttpResponseWrapper) response).getResponseBody();
+
+            if(responseBody!=null && responseBody.length>0){
+                log.info("Response Body: [{}]", new String(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectMapper.readValue(responseBody,Object.class))));
+            }
+
         }
+
+
 
 
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
